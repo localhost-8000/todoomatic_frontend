14179399e5415c13862c61d54cb6d87c75f3bed0
Rahul Tiwari <rahultwr0005@gmail.com>
2022-04-26 01:50:03 +0530

    fixed package versions

--- src/components/home/Task.tsx
+++ src/components/home/Task.tsx
@@ -6,9 +6,9 @@ export default function Task() {
         <div className="mt-20">
             <h2 className="text-xl font-bold text-dark-gray">My Tasks</h2>
             <div className="flex justify-start mt-3 border-b-2 border-b-gray-500">
-                <a href="#" className="text-lg font-semibold text-gray-700 px-4 py-1 mr-4 border-b-4 border-b-dark-purple">To Do</a>
-                <a href="#" className="text-lg font-semibold text-gray-700 px-4 py-1 mx-2">In Progress</a>
-                <a href="#" className="text-lg font-semibold text-gray-700 px-4 py-1">Done</a>
+                <a href="/todo" className="text-lg font-semibold text-gray-700 px-4 py-1 mr-4 border-b-4 border-b-dark-purple">To Do</a>
+                <a href="/in-progress" className="text-lg font-semibold text-gray-700 px-4 py-1 mx-2">In Progress</a>
+                <a href="/done" className="text-lg font-semibold text-gray-700 px-4 py-1">Done</a>
             </div>
 
             <ul>
